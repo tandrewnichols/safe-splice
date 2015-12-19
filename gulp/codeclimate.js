c308fa7a069ee3377f1cc396eef5b0b59b1abe5a
Andrew Nichols <tandrewnichols@gmail.com>
2015-12-18 23:22:13 -0500

    Add codeclimate token and lock jshint at 2.8.0

--- gulp/codeclimate.js
+++ gulp/codeclimate.js
@@ -5,7 +5,7 @@ gulp.task('codeclimate', function() {
   if (process.version.indexOf('v4') > -1) {
     gulp.src('coverage/lcov.info', { read: false })
       .pipe(codeclimate({
-        token: ''
+        token: '2b31a8a43543590667aed914698057f37eceae7d53af6873b553f0aefd40f60d'
       }));
   }
 });
